5378815f0dc9c9ff273dbae53c1a0f53bb972802
nelsonfjunior <ferreiranelson123@hotmail.com>
2024-11-08 11:00:55 -0300

    new

--- src/main/java/com/tarefas/tarefas/segurity/CorsConfig.java
+++ src/main/java/com/tarefas/tarefas/segurity/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200, https://front-tarefas-rose.vercel.app/, https://front-tarefas-rose.vercel.app, https://nelsonfjunior.shop/, https://www.nelsonfjunior.shop/, https://nelsonfjunior.shop, https://www.nelsonfjunior.shop")
+                .allowedOrigins("http://localhost:4200, https://front-tarefas-liart.vercel.app, https://front-tarefas-liart.vercel.app/, https://nelsonfjunior.shop/, https://www.nelsonfjunior.shop/, https://nelsonfjunior.shop, https://www.nelsonfjunior.shop")
                 .allowedMethods("GET", "POST", "DELETE", "PUT");
     }
 }
